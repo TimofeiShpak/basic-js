73f34636d0aa901ef3ad3b4048de2f14790a339e
TimofeiShpak <60565887+TimofeiShpak@users.noreply.github.com>
2020-02-21 23:16:46 +0400

    docs: update what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,6 @@
 module.exports = function getSeason(date) {
   if (!date) 
-    return 'Unable to determine the time of year!'
+    return 'Unable to determine the time of year!';
   else if (Object.keys(date).length > 0) {
     throw new Error();
   }
@@ -9,6 +9,4 @@ module.exports = function getSeason(date) {
 	else if(value==2 || value ==3 || value == 4)return 'spring';
 	else if(value==5 || value ==6 || value == 7)return 'summer';
 	else if(value==8 || value ==9 || value == 10)return 'autumn';
-
-
 };
