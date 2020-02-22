f913028183acf9d5bd0f090ec411c764a4b8ca24
TimofeiShpak <60565887+TimofeiShpak@users.noreply.github.com>
2020-02-22 22:54:27 +0400

    docs: update file what-season for optimise

--- src/what-season.js
+++ src/what-season.js
@@ -1,9 +1,6 @@
 module.exports = function getSeason(date) {
-  if (!date) 
-    return 'Unable to determine the time of year!';
-  else if (Object.keys(date).length > 0) {
-    throw new Error();
-  }
+  if (!date) return 'Unable to determine the time of year!';
+  else if (Object.keys(date).length > 0)  throw new Error(); 
 	let value = date.getMonth();
 	if(value==11 || value ==0 || value == 1)return 'winter';
 	else if(value==2 || value ==3 || value == 4)return 'spring';
