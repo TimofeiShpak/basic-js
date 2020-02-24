3bae1705c924f469adac1647c4094560290e0531
TimofeiShpak <60565887+TimofeiShpak@users.noreply.github.com>
2020-02-24 17:02:42 +0400

    docs: change solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,16 @@
 module.exports = class DepthCalculator {
-    calculateDepth(arr) {
-    	if(Array.isArray(arr))return 1;
-    	return 0;
+    calculateDepth(arr,a=1,array=[]) {
+    	array.push(a);
+    	for(let i = 0; i<arr.length; i++){
+    		if(Array.isArray(arr[i]))this.calculateDepth(arr[i],a+1,array); 
+    	}
+    	let value = 0;
+        for (let j = 0; j < array.length; j++) {
+            value = Math.max(value,array[j]);
+        }
+        return value;    
     }
 };
+
+
+  
